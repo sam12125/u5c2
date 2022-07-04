2cdb7f9fa6aeb5e18f0f680c46615c2d19e96d08
Saiyam vaid <99545380+sam12125@users.noreply.github.com>
2022-07-04 15:59:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const authRouter = require("./routes/auth.routes")
 const taskRouter = require("./routes/task.routes")
 const connection = require("./db")
+const PORT = process.env.PORT || 8080;
 
 const app = express()
 app.use(express.json());
@@ -14,7 +15,7 @@ app.get("/" , (req,res) => {
 })
 
 
-app.listen(8080 , async() => {
+app.listen(PORT , async() => {
     try{
         await connection
         console.log("connected to db successfully")
